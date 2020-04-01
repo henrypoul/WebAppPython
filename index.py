df9d7d337efdde85d34954424612071cf0de4329
henrypoul <henry.poul@hotmail.com>
2020-03-31 23:09:15 -0500

    version 2

--- index.py
+++ index.py
@@ -2,15 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Creating simple Routes 
-@app.route('/test')
-def test():
-    return "Home Page"
-
-@app.route('/test/about/')
-def about_test():
-    return "About Page"
-
 # Routes to Render Something
 @app.route('/')
 def home():
@@ -19,6 +10,18 @@ def home():
 @app.route('/about', strict_slashes=False)
 def about():
     return render_template("about.html")
+
+@app.route('/services', strict_slashes=False)
+def services():
+    return render_template("services.html")
+
+@app.route('/projects', strict_slashes=False)
+def projects():
+    return render_template("projects.html")
+
+@app.route('/contacts', strict_slashes=False)
+def contacts():
+    return render_template("contacts.html")
 
 # Make sure this we are executing this file
 if __name__ == '__main__':
